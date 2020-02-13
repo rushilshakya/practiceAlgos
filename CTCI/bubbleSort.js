d04b4265161d518460248e44d69c50e449f6f772
Rushil Shakya <rushil.shakya@gmail.com>
2020-02-13 14:42:57 -0500

    slight optimization

--- CTCI/bubbleSort.js
+++ CTCI/bubbleSort.js
@@ -5,14 +5,21 @@ const swap = (array, i1, i2) => {
 };
 
 const sortByBubble = disArray => {
+  let isSorted = false;
   for (let i = disArray.length - 1; i > 0; i--) {
+    isSorted = true;
+
     for (let j = 0; j < i; j++) {
-      if (disArray[j] > disArray[j + 1]) swap(disArray, j, j + 1);
+      if (disArray[j] > disArray[j + 1]) {
+        swap(disArray, j, j + 1);
+        isSorted = false;
+      }
     }
+    if (isSorted) break;
   }
   return disArray;
 };
 
-const disArray1 = [10, 5, 1, 3, 7, 5, 9, 2, 4, 6, 0];
+const disArray1 = [5, 1, 3, 7, 5, 9, 2, 4, 6];
 
 console.log(sortByBubble(disArray1));
