339e420b56fd35f63e3ded5da6b8b224ab09180b
Rushil Shakya <rushil.shakya@gmail.com>
2020-02-18 16:30:20 -0500

    getting started with the dynamic approach

--- CTCI/combinationChange.js
+++ CTCI/combinationChange.js
@@ -27,7 +27,7 @@ const coinCombosRec = (val, index = 0) => {
   return ways;
 };
 
-const coinCombosDyn = val => {
+const coinCombosDynOld = val => {
   let allCombos = {
     0: {},
   };
@@ -49,7 +49,29 @@ const coinCombosDyn = val => {
   return allCombos;
 };
 
-const val = 25;
+const coinCombosDyn = val => {
+  let allCombos = {
+    0: {},
+  };
+  for (let i = 1; i <= val; i++) {
+    //at the current val, lets go through all the coins
+    let combos = 0;
+    for (let j = 0; j < coins.length; j++) {
+      //to look at the combos minus this coin
+      let minusCoin = i - coins[j];
+      //if the remaining val is more than equal to one
+      //then add that to our running count
+      if (minusCoin >= 0) {
+        combos += allCombos[minusCoin];
+      }
+    }
+    //now lets populate all combos for this val
+    allCombos[i] = combos;
+  }
+  return allCombos[val];
+};
+
+const val = 10;
 // console.log(coinCombosDyn(val));
 console.log(coinCombosRec(val));
 // console.log(recMemo);
